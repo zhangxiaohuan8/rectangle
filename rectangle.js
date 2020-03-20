f2d5bbca5e2a969e2e7b77d5233216edde970425
zhangxiaohuan8 <2916244782@qq.com>
2020-03-20 11:04:10 +0800

    v05

--- rectangle.js
+++ rectangle.js
@@ -4,16 +4,12 @@ $(function(){
         $btnCal=$('#calculate'),
         $perimeter=$('#perimeter'),
         $area=$('#area');
-    function roundFractional(x, n) {
-        return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
-    }
-    $btnCal.click(function(){
-        var w= Number($width.val()),
-            h=Number($height.val());
-            var p=(roundFractional(2*(w+h),2)),
-            a=roundFractional(w*h,2);
-            console.log(p,a);
-            $perimeter.val(p);
-            $area.val(a);
-    });
+        $btnCal.click(function(){
+            var w= $width.val(),
+                h=$height.val();	
+            var r = new Rectangle(w, h);
+            $perimeter.val(r.perimeter());
+            $area.val(r.area());
+
+        });		
 }); 
