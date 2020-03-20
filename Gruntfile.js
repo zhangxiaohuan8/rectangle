091161ef52e225f8a53932aa82f845ac3e3538c8
zhangxiaohuan8 <2916244782@qq.com>
2020-03-20 10:30:57 +0800

    v04

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,11 +18,35 @@ module.exports = function (grunt) {
         },
         target: ['*.js']
       },
+      htmlmin: {
+        options: {
+          collapseWhitespace: true,
+          preserveLineBreaks: false
+        },
+        files: {
+          src: './index.html',
+          dest: 'dist/index.html'
+        }
+      },
+      cssmin: {
+        'dist/rectangle.css': 'rectangle.css'
+      },
+      uglify: {
+        release:{
+          files: {
+            'dist/rectangle.js': 'rectangle.js',
+          }
+        }
+      }	    
   
     });
   
     grunt.loadNpmTasks('grunt-htmlhint');
     grunt.loadNpmTasks('grunt-contrib-csslint');
     grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.registerTask('lint', ['htmlhint', 'csslint', 'eslint']);
+    grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
   };
